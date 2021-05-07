eb3eeb5c6117d8c85d3507ce3ccb2577ece1a682
Kris <kriskuiper74@gmail.com>
2021-05-07 09:23:40 +0200

    feat: add a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,9 @@
 module.exports = {
   stories: ['../src/docs/*.stories.mdx', '../src/components/**/*.stories.jsx'],
   addons: [
+    '@storybook/addon-a11y',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    'storybook-css-modules-preset'
+    'storybook-css-modules-preset',
   ]
 }
