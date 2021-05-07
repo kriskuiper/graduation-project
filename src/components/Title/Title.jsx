a3d414952c46cc94f9ca2e24be7d5ef62a3418f7
Kris <kriskuiper74@gmail.com>
2021-05-07 09:23:33 +0200

    feat: document Title component

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+/**
+ * This component enhances the default `h1` HTML tag by adding some accessibility sauce to it.
+ */
 const Title = ({ text }) => (
   <h1>
     {text}
   </h1>
 );
+
+Title.propTypes = {
+  /**
+   * Text to show inside the title
+   */
+  text: PropTypes.string.isRequired,
+};
 
 export default Title;
