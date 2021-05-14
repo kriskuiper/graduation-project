22ddefed0be2f5d34dfd3a87df39a70c71d94267
Kris <kriskuiper74@gmail.com>
2021-05-14 11:34:25 +0200

    feat: remove implementation from index page

--- demo/pages/index.jsx
+++ demo/pages/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Head from 'next/head';
-import { Dialog } from '@logiq/library';
 
 import styles from '../styles/Home.module.css';
 
@@ -8,9 +7,6 @@ export default function Home() {
   return (
     <>
       <h1>A Logiq dialog component</h1>
-      <Dialog className="">
-        Some content
-      </Dialog>
     </>
-  )
+  );
 }
