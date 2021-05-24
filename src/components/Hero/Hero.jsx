3942de847d10ddf65057434fe8eec3e53d374985
Kris Kuiper <kris@orangetalent.nl>
2021-05-24 17:36:46 +0200

    feat: add documentation to newest components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,6 +12,10 @@ const calculatePaddingBottom = (aspectRatio) => {
   return 100 / (heightNumber / widthNumber);
 };
 
+/**
+ * Hero component with a background image and a title inside of it. Also has the possibility
+ * to use some aspect ratio.
+ */
 const Hero = ({
   tag = 'section',
   backgroundImage,
@@ -67,27 +71,61 @@ const Hero = ({
 };
 
 Hero.propTypes = {
+  /**
+   * The HTML tag to use for the Hero component, defaults to `section`
+   */
   tag: PropTypes.string,
+
+  /**
+   * Url of the background image of the Hero component
+   */
   backgroundImage: PropTypes.string,
+
+  /**
+   * Background color for when the image isn't loaded yet.
+   */
   backgroundColor: PropTypes.string,
+
+  /**
+   * Vertical alignment of content (nodes) inside the hero component
+   */
   verticalContentAlignment: PropTypes.oneOf([
     'top',
     'center',
     'bottom',
   ]),
+
+  /**
+   * Horizontal alignment of content (nodes) inside the hero component
+   */
   horizontalContentAlignment: PropTypes.oneOf([
     'left',
     'center',
     'right',
   ]),
+
+  /**
+   * Aspectratio of the Hero component (optional), default to 16/9
+   */
   aspectRatio: PropTypes.string,
+
+  /**
+   * Wether to use the aspect ratio or stick to a default height.
+   */
   useAspectRatio: PropTypes.bool,
+
+  /**
+   * HTML nodes inside the Hero component, should be of type HeroContent
+   */
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]),
 };
 
+/**
+ * Content wrapper for content inside the Hero component
+ */
 const HeroContent = ({ children, className, style }) => (
   <div className={className} style={style}>
     {children}
@@ -95,11 +133,22 @@ const HeroContent = ({ children, className, style }) => (
 );
 
 HeroContent.propTypes = {
+  /**
+   * HTML nodes inside the HeroContent component
+   */
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]),
+
+  /**
+   * CSS class(es) of the HeroContent component
+   */
   className: PropTypes.string,
+
+  /**
+   * Style object of the HeroContent component, should be a CSS in JS sort of object.
+   */
   style: PropTypes.object,
 };
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Navigation.module.css';
 
+/**
+ * This component helps you create a navigation list. You can use an item
+ * manually.
+ */
 const Navigation = ({
   children,
   className = '',
@@ -24,6 +28,9 @@ const Navigation = ({
   </nav>
 );
 
+/**
+ * An item in a navigation list. You can style it like you want.
+ */
 const NavigationItem = ({
   className = '',
   style,
@@ -38,15 +45,38 @@ const NavigationItem = ({
 );
 
 Navigation.propTypes = {
+  /**
+   * CSS class(es) of the `nav` element
+   */
   className: PropTypes.string,
+
+  /**
+   * Alignment of the list in the navigation
+   */
   itemAlignment: PropTypes.oneOf([
     'right',
     'center',
     'left',
   ]),
+
+  /**
+   * CSS class(es) of the `ul` element
+   */
   listClassName: PropTypes.string,
+
+  /**
+   * Style object for the `nav` element (should be a CSS in JS object)
+   */
   style: PropTypes.object,
+
+  /**
+   * Style object for the `ul` element (should be a CSS in JS object)
+   */
   listStyle: PropTypes.object,
+
+  /**
+   * HTML nodes inside the `ul`, should only be of type NavigationItem
+   */
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
@@ -54,8 +84,19 @@ Navigation.propTypes = {
 };
 
 NavigationItem.propTypes = {
+  /**
+   * CSS class(es) of the `li` element.
+   */
   className: PropTypes.string,
+
+  /**
+   * Style object for the `li` element (should be a CSS in JS object)
+   */
   style: PropTypes.object,
+
+  /**
+   * HTML nodes inside the NavigationItem component
+   */
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
